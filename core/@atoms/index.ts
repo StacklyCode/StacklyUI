e946dd1b5868e23e6b2679e99f009ed3050cbda2
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2022-07-04 21:18:27 -0700

    asdsa

--- core/@atoms/index.ts
+++ core/@atoms/index.ts
@@ -3,4 +3,5 @@ export { default as AtomIcon } from './AtomIcon';
 export { default as AtomInput } from './AtomInput';
 export { default as AtomLoader } from './AtomLoader';
 export { default as AtomText } from './AtomText';
+export { default as AtomLink } from './AtomLink';
 export { default as AtomWrapper } from './AtomWrapper';
