971a39faf77ba501666a7cb10f58f797a8a21f72
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2022-07-31 02:38:03 -0700

    asdsa

--- core/utils/isBackDark/index.ts
+++ core/utils/isBackDark/index.ts
@@ -1,12 +1,8 @@
-import train from "../train";
-import { NeuralNetwork } from "brain.js";
 
 type IOutput = {
   black: number;
   white: number;
 };
-const net = new NeuralNetwork();
-net.train(train);
 
 const hexToRgb = (hex) => {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -18,15 +14,12 @@ const hexToRgb = (hex) => {
 };
 
 const isBackDark = (hex) => {
-  const color = Object.keys(hexToRgb(hex)).reduce(
-    (acc, key) => ({ ...acc, [key]: hexToRgb(hex)[key] / 255 }),
-    {}
-  );
-  const output = net.run(color) as IOutput;
-  if (output.black > output.white) {
-    return "#535353";
-  }
-  return "#fff";
+  const color = hexToRgb(hex);
+  const isDark =
+    0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b <=
+    128;
+  return isDark ? "#535353" : "#ffffff";
+  
 };
 
 export default isBackDark;
