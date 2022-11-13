7501534e3c61fa0a0a572d19a5f95682cdb1346f
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2022-11-13 06:05:00 -0700

    asdas

--- core/utils/index.ts
+++ core/utils/index.ts
@@ -1,6 +1,9 @@
 export { default as ChangeBrightness } from './changeBrightness';
+export { default as ChangeTransparency } from './changeTransparency';
 export { default as CreateThemes } from './createThemes';
 export { default as Hash } from './hash';
 export { default as IsBackDark } from './isBackDark';
-export { default as Train } from './train';
+export { isBackDarkBoolean } from './isBackDark';
 export { default as randomNumberBetween } from './randomNumberBetween';
+export { default as Train } from './train';
+export { default as UseSetRef } from './useSetRef';
