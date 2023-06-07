ffcaba0cd9974272ba455119ba9f900cc939724e
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-06-07 12:44:16 -0700

    123213

--- core/components/atoms/AtomInput/inputError.tsx
+++ core/components/atoms/AtomInput/inputError.tsx
@@ -1,10 +1,10 @@
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 import { InputErrorStyled } from './styled';
 import { AtomInputTypes } from './types';
 import { FormikValues } from 'formik';
 import { get } from '../../../utils/tinyLodash';
 
-const validateErrors = (formik: FormikValues, id: string): string =>
+const validateErrors = (formik: FormikValues, id: string) =>
   (get(formik?.values, id) !== `` || get(formik?.touched, id)) &&
   get(formik?.errors, id)
     ? get(formik?.errors, id)
@@ -12,9 +12,8 @@ const validateErrors = (formik: FormikValues, id: string): string =>
 
 const InputTextError: FC<AtomInputTypes> = (props) => {
   const { formik, id } = props;
-  const error = useMemo(() => validateErrors(formik, id), [formik, id]);
   if (!formik) return null;
-  if (!error) return null;
+
   return (
     <InputErrorStyled {...props}>{validateErrors(formik, id)}</InputErrorStyled>
   );
