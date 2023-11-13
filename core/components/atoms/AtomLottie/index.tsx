6e011b2d36aea575b9828b109a8674d7d6388da9
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-11-13 11:06:57 -0700

    :sparkles: feat: Add check for undefined document in AtomLottie component

--- core/components/atoms/AtomLottie/index.tsx
+++ core/components/atoms/AtomLottie/index.tsx
@@ -4,6 +4,8 @@ import { AtomLottieTypes } from './types';
 import helloAnimation from '../../../animations/hello.json';
 
 const AtomLottie: FC<AtomLottieTypes> = (props) => {
+  if (typeof document === 'undefined') return null;
+
   return <Lottie loop={true} animationData={helloAnimation} {...props} />;
 };
 
